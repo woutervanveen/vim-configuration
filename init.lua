76a36249a8e436661d4ecc40bef3e6642d07c6ef
woutervanveen <wouter.van.veen@dhl.com>
2024-04-06 08:42:14 +0200

    Java dev environment
    Need to fix scala in combination with java

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+require("java").setup()
+require("lspconfig").jdtls.setup({})
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,11 +13,11 @@ require("lazy").setup({
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.java" },
+    -- { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.editor.aerial" },
-    { import = "lazyvim.plugins.extras.lang.scala" },
+    -- { import = "lazyvim.plugins.extras.lang.scala" },
     -- { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
+    -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
